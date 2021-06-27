8800889bdbc30b52fe7f1cc458289d60d3b9bfd0
AlexCZhou <zbc1357@hotmail.com>
2021-06-27 23:19:32 +0800

    First game

--- Include/Game.h
+++ Include/Game.h
@@ -18,10 +18,26 @@ enum GameState {
     GAME_MENU,
     GAME_WIN
 };
+
+// Represents the four possible (collision) directions
+enum Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+
+// Defines a Collision typedef that represents collision data
+typedef std::tuple<bool, Direction, glm::vec2> Collision; // <collision?, what direction?, difference vector center - closest point>
+
 // Initial size of the player paddle
 const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
 // Initial velocity of the player paddle
 const float PLAYER_VELOCITY(500.0f);
+// Initial velocity of the Ball
+const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
+// Radius of the ball object
+const float BALL_RADIUS = 12.5f;
 // Game holds all game-related state and functionality.
 // Combines all game-related data into a single class for
 // easy access to each of the components and manageability.
@@ -43,8 +59,12 @@ public:
     void ProcessInput(float dt);
     void Update(float dt);
     void Render();
-    void ImguiInit(GLFWwindow* window);
+    void DoCollisions();
+    // reset
+    void ResetLevel();
+    void ResetPlayer();
 
+    void ImguiInit(GLFWwindow* window);
 private:
     // imgui
     
